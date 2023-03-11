e2adb2e5cc23bd5b751e0419d6180435a1c94585
Adil Alimzhanov <aalimzhanov@mta.ca>
2023-03-11 17:55:50 -0400

    front end

---
+++ front-end2/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import FrontController from './FrontController';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <FrontController />
+  </React.StrictMode>
+);
