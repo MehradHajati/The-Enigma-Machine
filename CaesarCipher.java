229551b9208e2901ee80074bbe4c35ad52c7c075
MehradHajati <79949480+MehradHajati@users.noreply.github.com>
2023-03-11 18:00:11 -0400

    Update CaesarCipher.java

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -2,7 +2,6 @@
 // Mehrad Hajati
 // 2021/05/29 
 // This program uses the Caesar Cipher to encrypt and decrypt messages.
-// At the end of the encryption a "-1" is added to the end to show the end of the message. 
 
 
 import java.util.*;
@@ -10,7 +9,7 @@ import java.util.*;
 public class CaesarCipher {
 
     // Constants 
-    public static final String ALPHABET = " abcdefghijklmnopqrstuvwxyz" ;
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz" ;
     public static final int ALPHABET_LENGTH = 26;
     // the string and integer beneath can be any negative number or any number larger than 27 but they must match each other for the program to work
 
@@ -21,7 +20,7 @@ public class CaesarCipher {
         message = message.toLowerCase();
         for (int i = 0; i < message.length(); i++){
             char letter = message.charAt(i);
-            int index = (ALPHABET.indexOf(letter) + (key % 27) )% 27;
+            int index = modAlphabetLength(ALPHABET.indexOf(letter) + key);
             // To avoid indexOutOfBounds, and also to skip the empty space
             encryptedMessage = encryptedMessage + ALPHABET.charAt(index);
         } 
@@ -30,10 +29,11 @@ public class CaesarCipher {
 
     //Decryption Method
     public static String decrypt(String letters, int key){
+        key = modAlphabetLength(key);
         letters = letters.toLowerCase();
         String message = "";
         for (int i = 0; i < letters.length(); i++){
-            int index = (ALPHABET.indexOf(letters.charAt(i)) - (key % 27));
+            int index = modAlphabetLength(ALPHABET.indexOf(letters.charAt(i)) - (key));
             // To avoid indexOutOfBounds
             if(index < 0 ){
                 index += 27;
