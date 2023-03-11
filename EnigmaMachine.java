07050629d852310055f25389777c6bbc96fd5068
MehradHajati <79949480+MehradHajati@users.noreply.github.com>
2023-03-11 16:19:27 -0400

    put model into enigma

--- EnigmaMachine.java
+++ EnigmaMachine.java
@@ -4,10 +4,34 @@
  */
 public class EnigmaMachine{
 
-    ModelManager model = null;
+    boolean encoding = true; // this instance variable will keep track of whether we are encoding or deconding, if true its we are encoding, false otherwise
 
 
     public EnigmaMachine(){
-        model = new ModelManager(this) ;
+        
     }
+
+    // Wrapper method for Vigenere Class
+    public String encryptVigenere(String plaintText, String key){ return VigenereCipher.encrypt(plaintText, key); }
+    public String decryptVigenere(String cipherText, String key){ return VigenereCipher.decrypt(cipherText, key); }
+
+    // Wrapper methods for MatrixCipher class
+    public String encryptMatrix(String plaintText, String key){ return MatrixCipher.encrypt(plaintText, key); }
+    public String decryptMatrix(String cipherText, String key){ return MatrixCipher.decrypt(cipherText, key); }
+
+    // Wrapper methods for RailFenceCipher class
+    public String encryptRaileFence(String plainText, int key){ return RailFenceCipher.encrypt(plainText, key); }
+    public String decryptRailFence(String cipherText, int key){ return RailFenceCipher.decrypt(cipherText, key); }
+
+    // Wrapper methods for AtbashCipher class
+    public String encryptAtbash(String plainText){ return AtbashCipher.encrypt(plainText); }
+    public String decryptAtbash(String cipherText){ return AtbashCipher.decrypt(cipherText); }
+
+    // Wrapper methods for AffineCipher class
+    public String encryptAffine(String plainText, int a, int b){ return AffineCipher.encrypt(plainText, a, b); }
+    public String decryptAffine(String cipherText, int a, int b){ return AffineCipher.decrypt(cipherText, a, b); }
+
+    // Wrapper methods for CaesarCipher class
+    public String encryptCaesar(String plainText, int key){ return CaesarCipher.encrypt(plainText, key); }
+    public String decryptCaesar(String cipherText, int key){ return CaesarCipher.decrypt(cipherText, key); }
 }
